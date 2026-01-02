202844714a720e866ee501f6ef86db6f941528db
Molodoy667 <molodoy667@gmail.com>
2026-01-03 01:25:11 +0200

    fix: TypeScript error in spy-get-channel-info

--- api/spy-get-channel-info.ts
+++ api/spy-get-channel-info.ts
@@ -55,7 +55,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       if (channel_identifier.startsWith('+')) {
         console.log('[Spy Get Channel Info] Invite link detected, joining channel...');
         try {
-          const result = await client.invoke(
+          const result: any = await client.invoke(
             new Api.messages.ImportChatInvite({
               hash: channel_identifier.substring(1), // Remove '+'
             })
